7235334a3ac7bf08436d712d6855c82d6c1ab799
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-06-21 20:19:51 -0600

    ✨ Basic data layout

--- src/pages/dashboard/[battleAcc].tsx
+++ src/pages/dashboard/[battleAcc].tsx
@@ -1,7 +1,8 @@
-import { Loading } from '@nextui-org/react'
+import { Loading, Table, Text } from '@nextui-org/react'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { map } from 'zod'
 import AccessDenied from '../../components/access-denied'
 import UserInfo from '../../components/user-info'
 import { trpc } from '../../utils/trpc'
@@ -16,8 +17,6 @@ const BattleAccount: NextPage = () => {
     'quick-match',
     { userId: session?.userId as string, battleAccName: battleAcc as string },
   ])
-
-  console.log(quickMatches.data)
 
   const exampleSubmit = async () => {
     createMatch.mutate({
@@ -48,7 +47,29 @@ const BattleAccount: NextPage = () => {
   return (
     <>
       <UserInfo session={session} status={status} />
-      <p> battleAcc: {battleAcc} </p>
+      <Text h2>{battleAcc}</Text>
+      {quickMatches.data?.match ? (
+        <Table aria-label="Example table with static content">
+          <Table.Header>
+            <Table.Column>Result</Table.Column>
+            <Table.Column>Role</Table.Column>
+            <Table.Column>Hero</Table.Column>
+            <Table.Column>Map Type</Table.Column>
+            <Table.Column>Map</Table.Column>
+          </Table.Header>
+          <Table.Body>
+            {quickMatches.data?.match.map((match) => (
+              <Table.Row key={match.id}>
+                <Table.Cell>{match.result}</Table.Cell>
+                <Table.Cell>{match.role}</Table.Cell>
+                <Table.Cell>{match.hero}</Table.Cell>
+                <Table.Cell>{match.mapType}</Table.Cell>
+                <Table.Cell>{match.map}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      ) : null}
     </>
   )
 }
