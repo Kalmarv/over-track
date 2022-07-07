2c5c4cbcc26b724e8ba9731018a76b590167ba69
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-07-06 20:45:09 -0600

    💄 Update table style

--- src/components/match-table.tsx
+++ src/components/match-table.tsx
@@ -1,4 +1,4 @@
-import { Button, Loading, Table, Text } from '@nextui-org/react'
+import { Button, Grid, Loading, Table, Text } from '@nextui-org/react'
 import { FC } from 'react'
 import { mapData } from '../constants'
 import { QuickMatch, trpc } from '../utils/trpc'
@@ -29,61 +29,69 @@ export const MatchTable: FC<{
   return (
     <>
       {matchData?.match.length ? (
-        <Table aria-label='Example table with static content'>
-          <Table.Header>
-            <Table.Column>Result</Table.Column>
-            <Table.Column>Role</Table.Column>
-            <Table.Column>Hero</Table.Column>
-            <Table.Column>Map Type</Table.Column>
-            <Table.Column>Map</Table.Column>
-            <Table.Column>Date</Table.Column>
-            <Table.Column>Delete</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {matchData?.match.map((match: any) => (
-              <Table.Row key={match.id}>
-                <Table.Cell>
-                  <MatchResult result={match.result} />
-                </Table.Cell>
-                <Table.Cell>
-                  {match.role === 'TANK' ? (
-                    <TankIcon />
-                  ) : match.role === 'SUPPORT' ? (
-                    <SupportIcon />
-                  ) : (
-                    <DamageIcon />
-                  )}
-                </Table.Cell>
-                <Table.Cell>
-                  <MatchHeroListing
-                    key={match.id + match.role}
-                    heroes={match.hero}
-                    role={match.role}
-                  />
-                </Table.Cell>
-                <Table.Cell>{match.mapType}</Table.Cell>
-                <Table.Cell>
-                  {mapData.filter((map) => map.value === match.map)?.[0]?.label}
-                </Table.Cell>
-                <Table.Cell>
-                  {match.playedAt.toLocaleString('en-us', {
-                    weekday: 'short',
-                    month: 'short',
-                    day: 'numeric',
-                  })}
-                </Table.Cell>
-                <Table.Cell>
-                  <Button
-                    auto
-                    light
-                    onClick={() => deleteQuickMatch.mutate({ quickMatchId: match.id })}>
-                    <DeleteIcon />
-                  </Button>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+        <div className='flex flex-row justify-center min-w-full mt-8'>
+          <Table
+            aria-label='Example table with static content'
+            containerCss={{
+              height: 'auto',
+              width: '100%',
+              maxWidth: '48rem',
+            }}>
+            <Table.Header>
+              <Table.Column>Result</Table.Column>
+              <Table.Column>Role</Table.Column>
+              <Table.Column>Hero</Table.Column>
+              <Table.Column>Map Type</Table.Column>
+              <Table.Column>Map</Table.Column>
+              <Table.Column>Date</Table.Column>
+              <Table.Column>Delete</Table.Column>
+            </Table.Header>
+            <Table.Body>
+              {matchData?.match.map((match: any) => (
+                <Table.Row key={match.id}>
+                  <Table.Cell>
+                    <MatchResult result={match.result} />
+                  </Table.Cell>
+                  <Table.Cell>
+                    {match.role === 'TANK' ? (
+                      <TankIcon />
+                    ) : match.role === 'SUPPORT' ? (
+                      <SupportIcon />
+                    ) : (
+                      <DamageIcon />
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <MatchHeroListing
+                      key={match.id + match.role}
+                      heroes={match.hero}
+                      role={match.role}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{match.mapType}</Table.Cell>
+                  <Table.Cell>
+                    {mapData.filter((map) => map.value === match.map)?.[0]?.label}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {match.playedAt.toLocaleString('en-us', {
+                      weekday: 'short',
+                      month: 'short',
+                      day: 'numeric',
+                    })}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Button
+                      auto
+                      light
+                      onClick={() => deleteQuickMatch.mutate({ quickMatchId: match.id })}>
+                      <DeleteIcon />
+                    </Button>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
       ) : (
         <Text h3 className='text-center m-16'>
           No matches! try adding one
